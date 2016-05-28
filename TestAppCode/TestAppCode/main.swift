c37d8dceafcf8d20f584f37f4b27c2fe18b466a6
Peter <peter.pan@utah.edu>
2016-05-27 21:21:16 -0600

    Added AppCode files.

---
+++ TestAppCode/TestAppCode/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  TestAppCode
+//
+//  Created by Peter Pan on 5/27/16.
+//  Copyright (c) 2016 thePeterPan. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!")
+
